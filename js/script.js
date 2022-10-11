3484ccb83365504906de78de17257aa41dae409c
pietrobrighella <info@pietrobrighella.com>
2022-10-11 21:32:19 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -12,6 +12,10 @@ const listaEmail = ['info@pietrobrighella.com'];
 const btnEmail = document.getElementById('btnVerificaEmail');
 btnEmail.addEventListener('click', verificaEmail);
 
+function reload() {
+    location.reload();
+  }
+
 function verificaEmail() {
     let myAlert = document.getElementById('myAlert');
     let warningAlert = document.getElementById('warningAlert');
@@ -24,26 +28,63 @@ function verificaEmail() {
             console.log(lancioDadiUtente);
             let lancioDadiComputer = Math.floor(Math.random() * (6 - 1 + 1) ) + 1;
             if (lancioDadiUtente > lancioDadiComputer){
-                console.log('You Win!');
+                let myGame = document.querySelector('.myContainer');
+                myGame.innerHTML = `
+                <div class="row row-cols-2 text-center">
+                    <div class="col display-1 fw-bold bg-info">You<br>${lancioDadiUtente}</div>
+                    <div class="col display-1 fw-bold bg-secondary text-light">Cpu<br>${lancioDadiComputer}</div>
+                </div>
+                <div class="row alert alert-success mt-3">Hai vinto!</div>
+                <div class="row">
+                    <button id="btnGiocaDiNuovo" class="btn btn-primary btn-lg mb-3">Rilancia di nuovo i dadi</button>
+                </div>
+                `
+                const btnGiocaDiNuovo = document.getElementById('btnGiocaDiNuovo');
+                btnGiocaDiNuovo.addEventListener('click', reload);
             } else if (lancioDadiUtente < lancioDadiComputer){
-                console.log('You Lose!');
+                let myGame = document.querySelector('.myContainer');
+                myGame.innerHTML = `
+                <div class="row row-cols-2 text-center">
+                    <div class="col display-1 fw-bold bg-info">You<br>${lancioDadiUtente}</div>
+                    <div class="col display-1 fw-bold bg-secondary text-light">Cpu<br>${lancioDadiComputer}</div>
+                </div>
+                <div class="row alert alert-danger mt-3">Hai perso!</div>
+                <div class="row">
+                    <button id="btnGiocaDiNuovo" class="btn btn-primary btn-lg mb-3">Rilancia di nuovo i dadi</button>
+                </div>
+                `
+                const btnGiocaDiNuovo = document.getElementById('btnGiocaDiNuovo');
+                btnGiocaDiNuovo.addEventListener('click', reload);
             } else {
-                console.log('Pareggio');
+                let myGame = document.querySelector('.myContainer');
+                myGame.innerHTML = `
+                <div class="row row-cols-2 text-center">
+                    <div class="col display-1 fw-bold bg-info">You<br>${lancioDadiUtente}</div>
+                    <div class="col display-1 fw-bold bg-secondary text-light">Cpu<br>${lancioDadiComputer}</div>
+                </div>
+                <div class="row alert alert-warning mt-3">Ops! Pareggio ...</div>
+                <div class="row">
+                    <button id="btnGiocaDiNuovo" class="btn btn-primary btn-lg mb-3">Rilancia di nuovo i dadi</button>
+                </div>
+                `
+                const btnGiocaDiNuovo = document.getElementById('btnGiocaDiNuovo');
+                btnGiocaDiNuovo.addEventListener('click', reload);
             }
         } else {
             const newAlert = document.createElement('div');
             newAlert.classList.add('alert','alert-warning','d-grid');
             newAlert.innerHTML = `
+            Spiacente, la tua email non è presente!<br>
             Clicca sul pulsante per registrarti e giocare.
-            <button class="btn btn-warning">Registrati ora!</button>
-            `;
-            myAlert.append(newAlert);
-            const newWarningAlert = document.createElement('div');
-            newWarningAlert.classList.add('alert','alert-danger');
-            newWarningAlert.innerHTML = `
-            <div class="alert alert-danger mt-3">Spiacente! <strong>${emailInserita}</strong> non è presente.</div>
+            <button id="btnRegistra" class="btn btn-warning mt-3">Registra l'email e riprova</button>
             `
-            warningAlert.append(newWarningAlert);
+            myAlert.append(newAlert);
+            
+            let btnRegistra = document.getElementById('btnRegistra');
+
+            btnRegistra.addEventListener('click', function nuovaEmail() {listaEmail.push(emailInserita);}, reload);
         }
     }
 }
+
+console.log(listaEmail);
